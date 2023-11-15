33e3f3b3d2133ebf81fd3e695a7c6c968836795d
Kyle Hardinge <52141268+kylehardinge@users.noreply.github.com>
2023-11-14 23:18:13 -0500

    api endpoint somewhat setup

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"net/http"
 	"html/template"
-	
+		
 	"io"
 	"log"
 	"path"
+	"fmt"
 
 	"github.com/labstack/echo/v4"
 )
@@ -27,6 +28,35 @@ func Index(c echo.Context) error {
 func Play(c echo.Context) error {
 	return c.Render(http.StatusOK, "play.html", nil)
 }
+
+func Random(c echo.Context) error {
+	return c.String(http.StatusOK, "play.html")
+}
+
+func Api(c echo.Context) error {
+	
+	verseBook := c.QueryParam("book")
+	verseChapter := c.QueryParam("chapter")
+	verseVerse := c.QueryParam("verse")
+	
+	dataType := c.Param("verse")
+
+	if dataType == "string" {
+		return c.String(http.StatusOK, fmt.Sprintf("you are requesting the verse %s %s:%s\n", verseBook, verseChapter, verseVerse))
+	}
+
+	if dataType == "json" {
+		return c.JSON(http.StatusOK, map[string]string{
+			"book": verseBook,
+			"chapter": verseChapter,
+			"verse": verseVerse,
+			"content": "For God so loved the world...",
+		})
+	}
+
+	return c.JSON(http.StatusBadRequest, map[string]string { "error": dataType })
+}
+
 func main() {
 	// Parse templates
 	templates, err := template.ParseGlob(path.Join(template_files, "*.html"))
@@ -46,6 +76,8 @@ func main() {
 
 	e.GET("/", Index)
 	e.GET("/play", Play)
+	e.GET("/random", Random)
+	e.GET("/api/:verse", Api)
 
 	e.Logger.Fatal(e.Start("localhost:42069"))
 }
