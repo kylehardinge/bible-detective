e0bab4aa632c29a301776b6dc2799cd166301c6c
Kyle Hardinge <52141268+kylehardinge@users.noreply.github.com>
2023-11-22 12:02:43 -0500

    db working, api workingish and node_modules removed from git

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"net/http"
+	"bible-detective/site/pkg/router"
+	"bible-detective/site/pkg/db"
 	"html/template"
-		
 	"io"
 	"log"
 	"path"
-	"fmt"
 
 	"github.com/labstack/echo/v4"
 )
@@ -21,41 +20,6 @@ type TemplateRenderer struct {
 func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 	return t.template.ExecuteTemplate(w, name, data)
 }
-func Index(c echo.Context) error {
-	return c.Render(http.StatusOK, "index.html", nil)
-}
-
-func Play(c echo.Context) error {
-	return c.Render(http.StatusOK, "play.html", nil)
-}
-
-func Random(c echo.Context) error {
-	return c.String(http.StatusOK, "play.html")
-}
-
-func Api(c echo.Context) error {
-	
-	verseBook := c.QueryParam("book")
-	verseChapter := c.QueryParam("chapter")
-	verseVerse := c.QueryParam("verse")
-	
-	dataType := c.Param("verse")
-
-	if dataType == "string" {
-		return c.String(http.StatusOK, fmt.Sprintf("you are requesting the verse %s %s:%s\n", verseBook, verseChapter, verseVerse))
-	}
-
-	if dataType == "json" {
-		return c.JSON(http.StatusOK, map[string]string{
-			"book": verseBook,
-			"chapter": verseChapter,
-			"verse": verseVerse,
-			"content": "For God so loved the world...",
-		})
-	}
-
-	return c.JSON(http.StatusBadRequest, map[string]string { "error": dataType })
-}
 
 func main() {
 	// Parse templates
@@ -63,6 +27,8 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error loading templates: %v\n", err)
 	}
+	
+	db.InitDb()
 
 	e := echo.New()
 	
@@ -74,11 +40,11 @@ func main() {
 	e.Static("/css", "public/css")
 	e.Static("/img", "public/img")
 
-	e.GET("/", Index)
-	e.GET("/play", Play)
-	e.GET("/random", Random)
-	e.GET("/api/:verse", Api)
+	e.GET("/", router.Index)
+	e.GET("/play", router.Play)
+	e.GET("/random", router.Random)
+	e.GET("/api/:verse", router.Api)
 
-	e.Logger.Fatal(e.Start("localhost:42069"))
+	e.Logger.Fatal(e.Start("localhost:8080"))
 }
 
---
+++ pkg/db/index.go
@@ -0,0 +1,270 @@
+package db
+
+import (
+	"bufio"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	// "slices"
+	"sort"
+	"strings"
+	"errors"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type FileScanner struct {
+	io.Closer
+	*bufio.Scanner
+}
+
+type Verse struct {
+	Book_id string `json:"book_id"`
+	Book_name string `json:"book_name"`
+	Chapter int `json:"chapter"`
+	Verse int `json:"verse"`
+	Text string `json:"text"`
+}
+
+type RandomVerse struct {
+	Id int
+	Book_id string `json:"book_id"`
+	Book_name string `json:"book_name"`
+	Chapter int `json:"chapter"`
+	Verse int `json:"verse"`
+	Text string `json:"text"`
+}
+
+type Chapter struct {
+	Header string `json:"header"`
+	Footer string `json:"footer"`
+	Verses []Verse `json:"verses"`
+}
+
+type BibleManifest struct {
+	Version string
+	Books []BookManifest
+}
+
+type BookManifest struct {
+	Name string
+	Id string
+	Index string
+	Chapters []ChapterManifest
+}
+
+type ChapterManifest struct {
+	Number int
+	Verses int
+}
+
+// type Db struct {
+// 	
+// 	*sql.DB
+// }
+
+var Db *sql.DB
+
+func InitDb() {
+	db, err := sql.Open("mysql", "root:london-tasmania-broil-scorch-nov-chinese@tcp(localhost:3306)/bible_detective")
+
+	if err != nil {
+		fmt.Println("error validating sql.Open statement...")
+		panic(err.Error())
+	}
+
+	// defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("error verifying connection with db.Ping...")
+		panic(err.Error())
+	}
+
+	err = seedData(db)
+	if err != nil {
+		fmt.Println("error seeding the data in the db")
+		panic(err.Error())
+	}
+
+	fmt.Println("Connection successful to test database")
+	Db = db
+}
+
+func seedData(db *sql.DB) error {
+	result := db.QueryRow("SELECT table_name FROM information_schema.tables WHERE table_schema = DATABASE() AND table_name = 'kjv'")
+	
+	var name string
+	err := result.Scan(&name)
+	if err != nil && name == "" {
+		err = setupKjv(db)
+		if err != nil {
+			fmt.Println("error setting up the KJV database...")
+		}
+	}
+
+	return nil
+}
+
+func setupKjv(db *sql.DB) error {
+	// _, err := db.Exec(`DROP TABLE kjv`) 
+	// if err != nil {
+	// 	fmt.Println("error creating table...")
+	// 	panic(err.Error())
+	// }
+	_, err := db.Exec(`CREATE TABLE IF NOT EXISTS kjv (
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	book_id TEXT NOT NULL,
+	book_name TEXT NOT NULL,
+	chapter INTEGER NOT NULL,
+	verse INTEGER NOT NULL,
+	text TEXT NOT NULL
+	)`) 
+	if err != nil {
+		fmt.Println("error creating table...")
+		panic(err.Error())
+	}
+
+	bookDirectory := "assets/kjv/bible-json"
+	books, err := os.ReadDir(bookDirectory)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var bookList []string
+	for _, book := range books {
+		bookList = append(bookList, book.Name())
+	}
+	
+	correctBookOrder, correctBookSort, err := getBookOrder("assets/kjv/bookOrder.csv")
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println(correctBookOrder)
+
+	for i, book := range bookList {
+		if book != correctBookSort[i] {
+			fmt.Println(fmt.Sprintf("AHHH this book does not match %s <- err book | correct book -> %s", book, correctBookSort[i]))
+			return errors.New("Error in naming of books")
+		}
+	}
+	kjvManifest := BibleManifest{}
+	kjvManifest.Version = "KJV"
+	idNumber := 0
+	
+	for i, book := range correctBookOrder {
+		// fmt.Println(makeFilePath(bookDirectory, book.Name()))
+		fmt.Println(i, book)
+		chapters, err := os.ReadDir(makeFilePath(bookDirectory, book))
+		if err != nil {
+			panic(err.Error())
+		}
+		numChapters := len(chapters)	
+		for chapter := 1; chapter <= numChapters; chapter++ {
+			// fmt.Printf("--%s\n", chapter.Name())
+
+			contents, err := os.ReadFile(makeFilePath(bookDirectory, book, fmt.Sprintf("%d.json", chapter)))
+			if err != nil {
+				panic(err.Error())
+			}
+
+			chapter := Chapter{}
+				
+			json.Unmarshal(contents, &chapter)
+			
+			for _, verse := range chapter.Verses {
+				_, err := db.Exec(`INSERT INTO kjv (book_id, book_name, chapter, verse, text) VALUES (?,?,?,?,?);`, verse.Book_id, verse.Book_name, verse.Chapter, verse.Verse, verse.Text)
+				if err != nil {
+					panic(err.Error())
+				}
+				idNumber++
+			}
+		}
+	}
+	fmt.Println("Data has been entered")
+
+	return nil
+}
+
+// func setupKjvData(db *sql.DB) error {
+// 	_, err := db.Exec(`CREATE TABLE IF NOT EXISTS `)
+// 	if err != nil {
+// 		fmt.Println("error creating table...")
+// 		panic(err.Error())
+// 	}
+// 	
+// 	return nil
+// }
+
+func setupFbv(db *sql.DB) error {
+	_, err := db.Exec(`CREATE TABLE IF NOT EXISTS fbv (
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	book TEXT NOT NULL,
+	chapter INTEGER NOT NULL,
+	verse INTEGER NOT NULL,
+	content TEXT NOT NULL
+	)`) 
+	if err != nil {
+		fmt.Println("error creating table...")
+		panic(err.Error())
+	}
+	// reReference := regexp.MustCompile(`[A-Z]{3}\.[1-9]+\.[1-9]+`)
+	// reSplitter := regexp.MustCompile(`\.`)
+	reContent := regexp.MustCompile(`Content: (.+)`)
+	scanner := readByLine("pkg/parser/fbv.txt")
+
+	fmt.Println(scanner.Text())
+	for scanner.Scan() {
+		// fmt.Println(scanner.Text())
+		// reference := reSplitter.Split(string(reReference.Find([]byte(scanner.Text()))), -1)
+		content := reContent.FindSubmatch([]byte(scanner.Text()))
+		fmt.Println(string(content[1]))
+		// fmt.Println(string(re.Find([]byte(scanner.Text()))))
+	}
+	return nil	
+}
+
+func readByLine(filename string) *bufio.Scanner {
+	// fmt.Println(os.Getwd())
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("error opening Bible file...")
+	}
+	// defer file.Close()
+	
+	scanner := bufio.NewScanner(file)
+	// for scanner.Scan() {
+	// 	fmt.Print(scanner.Text());
+	// }
+	return scanner
+}
+
+func getKjvData(directory string) error {
+	return nil
+}
+
+func makeFilePath(part ...string) string {
+	filePath := ""
+	for i, fp := range part {
+		if i != 0 {
+			filePath += "/"
+		}
+		filePath += fp
+	}
+	return filePath
+}
+
+func getBookOrder(filepath string) ([]string, []string, error) {
+	content, err := os.ReadFile(filepath)
+	if err != nil {
+		panic(err.Error())
+	}
+	orderedBooks := strings.Split(strings.Split(string(content),"\n")[0], ",")
+	books := make([]string, len(orderedBooks))
+	copy(books, orderedBooks)
+	sort.Strings(orderedBooks)
+	return books, orderedBooks, nil
+}
